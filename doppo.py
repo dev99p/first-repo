088669899d49fa8b0ec1a1fa532f13a91c1de877
Dev1795 <2023ucp1795@mnit.ac.in>
2024-10-14 14:43:21 +0530

    PythonFileChanges

--- doppo.py
+++ doppo.py
@@ -4,4 +4,8 @@ vector1 = list(map(int, input().split())
 print("Enter the xyz-dimension of vector 2 separated by spaces: ")
 vector2 = list(map(int, input().split())
 
-print("The dot product of vector 1 and vector 2 is ", vector1[0]*vector2[0] + vector1[1]*vector2[1] + vector1[2]*vector2[2])
+int v_x = vector1[0]*vector2[0]
+int v_y = vector1[1]*vector2[1]
+int v_z = vector1[2]*vector2[2]
+
+print("The dot product of vector 1 and vector 2 is ", v_x + v_y + v_z)
